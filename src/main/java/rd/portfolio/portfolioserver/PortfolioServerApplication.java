2bc6ea2d68c191e72bdf827cbe67932d89ca22e2
Rachel Duret <rachel.duret@alphanetworks.tv>
2024-10-06 11:50:07 +0200

    init

---
+++ src/main/java/rd/portfolio/portfolioserver/PortfolioServerApplication.java
@@ -0,0 +1,13 @@
+package rd.portfolio.portfolioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rd/portfolio/portfolioserver/PortfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rd.portfolio.portfolioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
