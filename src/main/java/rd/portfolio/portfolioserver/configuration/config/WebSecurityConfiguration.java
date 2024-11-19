b0b2c8246bb93c9e75ad005b9b45a3d2b2d0d5b2
Rachel Duret <rachel.duret@alphanetworks.tv>
2024-11-19 09:52:52 +0100

    implement security

--- src/main/java/rd/portfolio/portfolioserver/configuration/config/WebSecurityConfiguration.java
+++ src/main/java/rd/portfolio/portfolioserver/configuration/config/WebSecurityConfiguration.java
@@ -13,8 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import rd.portfolio.portfolioserver.configuration.jwt.JwtAuthEntryPoint;
 import rd.portfolio.portfolioserver.configuration.jwt.JwtAuthenticationFilter;
+import rd.portfolio.portfolioserver.configuration.jwt.JwtUtil;
 import rd.portfolio.portfolioserver.service.UserDetailService;
 
 ;
@@ -25,29 +25,12 @@ import rd.portfolio.portfolioserver.service.UserDetailService;
 public class WebSecurityConfiguration {
 
     private final UserDetailService userSecurityService;
-    private final JwtAuthEntryPoint jwtAuthEntryPoint;
-    //    private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtUtil jwtUtil;
 
     @Bean
-    public static PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                    .cors(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(req -> req.requestMatchers("/auth/register")
-                                                     .permitAll()
-                                                     .anyRequest()
-                                                     .authenticated())
-                    .sessionManagement(ssession -> ssession.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    //                    .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        //                .exceptionHandling(ex->ex.authenticationEntryPoint(unauthorizedHandler))
-
-        return httpSecurity.build();
     }
 
     @Bean
@@ -55,6 +38,26 @@ public class WebSecurityConfiguration {
         AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder);
         return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(req -> req.requestMatchers("/auth/*")
+                                                     .permitAll()
+                                                     .anyRequest()
+                                                     .authenticated())
+                    //                    .formLogin(form -> form.loginPage("/login")
+                    //                                           .defaultSuccessUrl("/users")
+                    //                                           .loginProcessingUrl("/auth/login")
+                    //                                           .permitAll()
+                    //                              )
+                    .sessionManagement(ssession -> ssession.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    //                    .authenticationProvider(authenticationProvider)
+                    .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userSecurityService), UsernamePasswordAuthenticationFilter.class);
+        //                .exceptionHandling(ex->ex.authenticationEntryPoint(unauthorizedHandler))
+
+        return httpSecurity.build();
     }
 
 }
--- src/main/java/rd/portfolio/portfolioserver/configuration/jwt/JwtAuthenticationFilter.java
+++ src/main/java/rd/portfolio/portfolioserver/configuration/jwt/JwtAuthenticationFilter.java
@@ -10,12 +10,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import rd.portfolio.portfolioserver.service.UserDetailService;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 @RequiredArgsConstructor
 @Component
@@ -29,25 +27,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String token = getTokenFromRequest(request);
-        String username = jwtUtil.extractUsername(token);
-        UserDetails userDetails = userDetailService.loadUserByUsername(username);
-        if (StringUtils.hasText(token) && jwtUtil.validateToken(token, userDetails)) {
+        String headerAuth = request.getHeader("Authorization");
 
-            UsernamePasswordAuthenticationToken authenticationToken = new
-                    UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        String username = null;
+        String jwt = null;
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+            jwt = headerAuth.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = userDetailService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new
+                        UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
         }
         filterChain.doFilter(request, response);
 
-    }
-
-    private String getTokenFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
